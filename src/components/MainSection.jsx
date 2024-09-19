3005544b74c9d6af60e679baede6b881eb5df039
siva tharun <sivatharun2212@gmail.com>
2024-09-19 09:55:59 +0530

    added development nitice

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -4,6 +4,9 @@ const MainSection = () => {
 	return (
 		<div className="w-full mx-0 sm:mx-[18%] p-4 pr-[18%] sm:pr-0">
 			<Welcome />
+			<div className="py-20">
+				<h1 className="text-4xl font-bold text-white text-center">Development in progress </h1>
+			</div>
 		</div>
 	);
 };
